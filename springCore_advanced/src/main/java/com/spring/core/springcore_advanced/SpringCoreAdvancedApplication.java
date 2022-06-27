5baf5343db71ac0dfdbe7deea21451eee936aba5
Kim yonggwan <sssw0101@naver.com>
2022-06-27 22:48:38 +0900

    init

---
+++ springCore_advanced/src/main/java/com/spring/core/springcore_advanced/SpringCoreAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.spring.core.springcore_advanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCoreAdvancedApplication.class, args);
+    }
+
+}
---
+++ springCore_advanced/src/test/java/com/spring/core/springcore_advanced/SpringCoreAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.core.springcore_advanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreAdvancedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
