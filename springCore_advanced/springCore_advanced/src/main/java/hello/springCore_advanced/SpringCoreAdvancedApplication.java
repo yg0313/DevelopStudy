942c356ee948f15960106468e90158fd6ea06e63
Kim yonggwan <sssw0101@naver.com>
2022-06-27 23:22:41 +0900

    init

--- springCore_advanced/springCore_advanced/src/main/java/hello/springCore_advanced/SpringCoreAdvancedApplication.java
+++ springCore_advanced/springCore_advanced/src/main/java/hello/springCore_advanced/SpringCoreAdvancedApplication.java
@@ -1,4 +1,4 @@
-package com.spring.core.springcore_advanced;
+package hello.springCore_advanced;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringCoreAdvancedApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringCoreAdvancedApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreAdvancedApplication.class, args);
+	}
 
 }
--- springCore_advanced/springCore_advanced/src/test/java/hello/springCore_advanced/SpringCoreAdvancedApplicationTests.java
+++ springCore_advanced/springCore_advanced/src/test/java/hello/springCore_advanced/SpringCoreAdvancedApplicationTests.java
@@ -1,4 +1,4 @@
-package com.spring.core.springcore_advanced;
+package hello.springCore_advanced;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringCoreAdvancedApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
