3e0074358fa8c5380aa29bbfc3d814daa0c40517
Kim yonggwan <sssw0101@naver.com>
2022-06-29 23:06:18 +0900

    로그 추적기 - 요구사항 분석

--- springCore_advanced/springCore_advanced/src/main/java/hello/springCore_advanced/app/v0/OrderControllerV0.java
+++ springCore_advanced/springCore_advanced/src/main/java/hello/springCore_advanced/app/v0/OrderControllerV0.java
@@ -4,6 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 로그 추적기 - 요구사항분석.
+ * 모든 public 메소드의 호출과 응답 정보를 로그로 출력.
+ * 메소드 호출에 걸린 시간 체크.
+ * 정상흐름과 예외흐름 구분.
+ * 메소드 호출의 깊이 표현.
+ */
 @RestController
 @RequiredArgsConstructor
 public class OrderControllerV0 {
