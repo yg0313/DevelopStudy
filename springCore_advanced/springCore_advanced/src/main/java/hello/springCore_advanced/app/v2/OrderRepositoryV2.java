8fb45c50c2fa80f8f62130ead5e96009b5356cb3
Kim yonggwan <sssw0101@naver.com>
2022-07-01 00:15:40 +0900

    로그 추적기 V2 - 적용

---
+++ springCore_advanced/springCore_advanced/src/main/java/hello/springCore_advanced/app/v2/OrderRepositoryV2.java
@@ -0,0 +1,39 @@
+package hello.springCore_advanced.app.v2;
+
+import hello.springCore_advanced.trace.TraceId;
+import hello.springCore_advanced.trace.TraceStatus;
+import hello.springCore_advanced.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV2 {
+
+    private final HelloTraceV2 trace;
+
+    public void save(TraceId traceId, String itemId) {
+        TraceStatus status = null;
+
+        try {
+            status = trace.beginSync(traceId,"OrderRepositoryV1.save()");
+            if (itemId.equals("ex")) {
+                throw new IllegalStateException("예외 발생");
+            }
+            sleep(1000);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; //예외를 꼭 다시 던져주어야 한다.
+        }
+
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
