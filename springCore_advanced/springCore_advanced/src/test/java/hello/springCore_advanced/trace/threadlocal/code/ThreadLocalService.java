338921d19644b25e6647b646ffaf419305202953
Kim yonggwan <sssw0101@naver.com>
2022-07-03 15:02:31 +0900

    ThreadLocal - 예제 코드

---
+++ springCore_advanced/springCore_advanced/src/test/java/hello/springCore_advanced/trace/threadlocal/code/ThreadLocalService.java
@@ -0,0 +1,29 @@
+package hello.springCore_advanced.trace.threadlocal.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * ThreadLocal을 사용하여 동시성 문제 해결.
+ */
+@Slf4j
+public class ThreadLocalService {
+
+    private ThreadLocal<String> nameStore = new ThreadLocal<>();
+
+    public String logic(String name){
+        log.info("저장 name={} -> nameStore={}", name, nameStore.get());
+
+        nameStore.set(name);
+        sleep(1000);
+        log.info("조회 nameStore={}", nameStore.get());
+        return nameStore.get();
+    }
+
+    private void sleep(int millis) {
+        try{
+            Thread.sleep(millis);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
