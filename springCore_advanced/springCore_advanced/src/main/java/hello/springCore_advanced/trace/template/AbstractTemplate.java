95bddeb8fd0f98ade439d05d86a7da850fdd3504
Kim yonggwan <sssw0101@naver.com>
2022-07-08 23:35:52 +0900

    템플릿 메서드 패턴 - 적용2

--- springCore_advanced/springCore_advanced/src/main/java/hello/springCore_advanced/trace/template/AbstractTemplate.java
+++ springCore_advanced/springCore_advanced/src/main/java/hello/springCore_advanced/trace/template/AbstractTemplate.java
@@ -4,8 +4,9 @@ import hello.springCore_advanced.trace.TraceStatus;
 import hello.springCore_advanced.trace.logtrace.LogTrace;
 
 /**
- * 템플릿 메소드 패턴의 부모 클래스, 템플릿 역할
- * <T> 제네릭을 사용하여, 반환타입 정의
+ * 템플릿 메소드 패턴의 부모 클래스, 템플릿 역할.
+ * <T> 제네릭을 사용하여, 반환타입 정의.
+ * 단일 책임 원칙(SRP)을 잘 지킴 -> 변경 지점을 하나로 모아서 변경에 쉽게 대처 가능.
  */
 public abstract class AbstractTemplate<T> {
 
@@ -32,7 +33,7 @@ public abstract class AbstractTemplate<T> {
     }
 
     /**
-     * 해당 객체를 생성한 코드에서 메소드 재정의
+     * 해당 객체를 생성한 코드에서 메소드 재정의.
      */
     protected abstract T call();
 }
