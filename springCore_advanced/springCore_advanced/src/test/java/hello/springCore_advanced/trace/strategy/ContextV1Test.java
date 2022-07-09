288be0c19d268af200b32be40247e3629c82c2fa
Kim yonggwan <sssw0101@naver.com>
2022-07-09 23:22:18 +0900

    전략 패턴 - 시작

---
+++ springCore_advanced/springCore_advanced/src/test/java/hello/springCore_advanced/trace/strategy/ContextV1Test.java
@@ -0,0 +1,73 @@
+package hello.springCore_advanced.trace.strategy;
+
+import hello.springCore_advanced.trace.template.code.AbstractTemplate;
+import hello.springCore_advanced.trace.template.code.SubClassLogic1;
+import hello.springCore_advanced.trace.template.code.SubClassLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV1Test {
+    @Test
+    void templateMethodV0(){
+        logic1();
+        logic2();
+    }
+
+    private void logic1(){
+        long startTime= System.currentTimeMillis();
+        //비즈니스 로직 실행
+        log.info("비즈니스 로직1 실행");
+        //비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    private void logic2(){
+        long startTime= System.currentTimeMillis();
+        //비즈니스 로직 실행
+        log.info("비즈니스 로직2 실행");
+        //비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    /**
+     * 템플릿 메소드 패턴 적용.
+     */
+    @Test
+    void templateMethodV1(){
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.execute();
+
+        AbstractTemplate template2 = new SubClassLogic2();
+        template2.execute();
+    }
+
+    /**
+     * 익명 내부 클래스 사용.
+     * 클래스를 새로 생성하여 만들 필요 없음.
+     */
+    @Test
+    void templateMethodV2(){
+        AbstractTemplate template1 = new AbstractTemplate(){
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        log.info("클래스 이름1={}", template1.getClass());
+        template1.execute();
+
+        AbstractTemplate template2 = new AbstractTemplate(){
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+        log.info("클래스 이름2={}", template2.getClass());
+        template2.execute();
+    }
+}
