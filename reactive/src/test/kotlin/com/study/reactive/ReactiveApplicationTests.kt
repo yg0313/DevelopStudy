f907a33a0566ac9a2ca31e87fa7a4cef1d787107
Kim yonggwan <sssw0101@naver.com>
2022-08-14 15:45:14 +0900

    Reactive Study Init

---
+++ reactive/src/test/kotlin/com/study/reactive/ReactiveApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.reactive
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReactiveApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
