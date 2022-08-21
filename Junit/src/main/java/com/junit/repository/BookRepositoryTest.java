28a7b6b7731c9512217aa55ae2cf45e05cb2c338
Kim yonggwan <sssw0101@naver.com>
2022-08-22 00:24:02 +0900

    파일간 위치 수정

--- Junit/src/main/java/com/junit/repository/BookRepositoryTest.java
+++
@@ -1,4 +0,0 @@
-package com.junit.repository;
-
-public interface BookRepositoryTest {
-}
---
+++ Junit/src/test/java/com/junit/repository/BookRepositoryTest.java
@@ -0,0 +1,8 @@
+package com.junit.repository;
+
+import com.junit.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepositoryTest{
+
+}
