4c991636b946e4a5e4c45907fc635d50cbe9602d
Kim yonggwan <sssw0101@naver.com>
2022-08-22 00:21:26 +0900

    Book 엔티티 생성

--- Junit/src/main/java/com/junit/domain/Book.java
+++ Junit/src/main/java/com/junit/domain/Book.java
@@ -1,4 +1,31 @@
 package com.junit.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String title;
+    @Column(length = 20, nullable = false)
+    private String author;
+
+    @Builder
+    public Book(Long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+
 }
