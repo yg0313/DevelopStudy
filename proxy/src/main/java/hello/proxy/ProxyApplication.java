ac240b14d4b42ec242880e84e38562ab6e054c5c
Kim yonggwan <sssw0101@naver.com>
2022-08-25 00:22:23 +0900

    인터페이스 기반 프록시 - 적용

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -2,6 +2,7 @@ package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
 import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -11,7 +12,8 @@ import org.springframework.context.annotation.Import;
 
 
 //@Import(AppV1Config.class) //AppV1Config.class 를 스프링 빈으로 등록.
-@Import({AppV1Config.class, AppV2Config.class}) //AppV2Config.class 를 스프링 빈으로 등록.
+//@Import({AppV1Config.class, AppV2Config.class}) //AppV2Config.class 를 스프링 빈으로 등록.
+@Import(InterfaceProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //@Component 기능과 같다. 컴포넌트 스캔을 시작할 위치를 "hello.proxy.app"와 하위 패키지만 지정
 public class ProxyApplication {
 
