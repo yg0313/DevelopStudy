482bf567555d919ed29d31f6c96c759c5d743f0b
Kim yonggwan <sssw0101@naver.com>
2022-08-31 00:16:32 +0900

    JDK 동적 프록시 - 적용1
    todo
    - no-log 호출시 로그가 남는 문제 해결하기

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -2,6 +2,7 @@ package hello.proxy;
 
 import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -13,7 +14,8 @@ import org.springframework.context.annotation.Import;
 //@Import(AppV1Config.class) //AppV1Config.class 를 스프링 빈으로 등록.
 //@Import({AppV1Config.class, AppV2Config.class}) //AppV2Config.class 를 스프링 빈으로 등록.
 //@Import(InterfaceProxyConfig.class)
-@Import(ConcreteProxyConfig.class)
+//@Import(ConcreteProxyConfig.class)
+@Import(DynamicProxyBasicConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //@Component 기능과 같다. 컴포넌트 스캔을 시작할 위치를 "hello.proxy.app"와 하위 패키지만 지정
 public class ProxyApplication {
 
