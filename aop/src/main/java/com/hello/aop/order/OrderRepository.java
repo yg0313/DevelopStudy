7d81157eacdbe2065750513df0b5bd307b1adfd4
Kim yonggwan <sssw0101@naver.com>
2022-09-25 20:36:57 +0900

    예제 프로젝트 만들기

---
+++ aop/src/main/java/com/hello/aop/order/OrderRepository.java
@@ -0,0 +1,19 @@
+package com.hello.aop.order;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Slf4j
+public class OrderRepository {
+
+    public String save(String itemId){
+        log.info("[orderRepository] 실행");
+
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("예외 발생 : " + itemId);
+        }
+
+        return "ok "+ itemId;
+    }
+}
