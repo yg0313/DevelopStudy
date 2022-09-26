5452c808a189639e777da17abf72d78c6fe38276
Kim yonggwan <sssw0101@naver.com>
2022-09-26 22:48:55 +0900

    스프링 AOP 구현2 - 포인트컷 분리
    포인틔컷 메소드명 수정

--- aop/src/main/java/com/hello/aop/order/aop/AspectV2.java
+++ aop/src/main/java/com/hello/aop/order/aop/AspectV2.java
@@ -14,9 +14,9 @@ public class AspectV2 {
      * com.hello.aop.order 패키지와 하위 패키지.
      */
     @Pointcut("execution(* com.hello.aop.order..*(..))")
-    private void allaOrder(){}  // pointcut signature -> 메소드 이름과 파라미터를 합친것.
+    private void allOrder(){}  // pointcut signature -> 메소드 이름과 파라미터를 합친것.
 
-    @Around("allaOrder()")
+    @Around("allOrder()")
     public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable{
         log.info("[log] {}", joinPoint.getSignature()); //join point 시그니처
 
