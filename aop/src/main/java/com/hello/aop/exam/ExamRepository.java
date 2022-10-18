3e60b458cea048c427469d4a48e070380d8237b6
Kim yonggwan <sssw0101@naver.com>
2022-10-19 00:02:11 +0900

    스프링 AOP 실전 예제 - 재시도 AOP

--- aop/src/main/java/com/hello/aop/exam/ExamRepository.java
+++ aop/src/main/java/com/hello/aop/exam/ExamRepository.java
@@ -1,5 +1,6 @@
 package com.hello.aop.exam;
 
+import com.hello.aop.exam.annotation.Retry;
 import com.hello.aop.exam.annotation.Trace;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +13,7 @@ public class ExamRepository {
      * 5번에 1번 실패하는 요청
      */
     @Trace
+    @Retry(value = 4)
     public String save(String itemId){
         seq++;
         if(seq % 5 == 0){
