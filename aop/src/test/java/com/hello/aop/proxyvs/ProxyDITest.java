2a72906efeaca16f401001146c2852906d6a33fd
Kim yonggwan <sssw0101@naver.com>
2022-10-28 20:46:25 +0900

    프록시 기술과 한계 - 의존관계 주입 (CGLIB 프록시)

--- aop/src/test/java/com/hello/aop/proxyvs/ProxyDITest.java
+++ aop/src/test/java/com/hello/aop/proxyvs/ProxyDITest.java
@@ -10,23 +10,25 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @Slf4j
-@SpringBootTest(properties = {"spring.aop.proxy-target-class=false"}) //JDK 동적 프록시
+//@SpringBootTest(properties = {"spring.aop.proxy-target-class=false"}) //JDK 동적 프록시
+@SpringBootTest(properties = {"spring.aop.proxy-target-class=true"}) //CGLIB 프록시
 @Import(ProxyDIAspect.class)
 public class ProxyDITest {
 
+    /**
+     * CGLIB 프록시는 구체클래스를 기반으로 만들어지므로,
+     * 구체클래스를 상속한 인터페이스 상속도 가능하다.
+     */
     @Autowired
     MemberService memberService;
 
-    /**
-     * JDK 동적 프록시에서는 인터페이스가 아닌, 구체클래스타입 의존관계 주입 불가능.
-     */
-//    @Autowired
-//    MemberServiceImpl memberServiceImpl;
+    @Autowired
+    MemberServiceImpl memberServiceImpl;
 
     @Test
     void go(){
         log.info("memberService class={}", memberService.getClass());
-//        log.info("memberServiceImpl class={}", memberServiceImpl.getClass());
-//        memberServiceImpl.hello("hello");
+        log.info("memberServiceImpl class={}", memberServiceImpl.getClass());
+        memberServiceImpl.hello("hello");
     }
 }
