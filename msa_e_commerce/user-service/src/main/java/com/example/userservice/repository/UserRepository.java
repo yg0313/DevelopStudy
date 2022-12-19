9cb398db5b1429291bfb5fee93e67e0b7f3ef36e
Kim yonggwan <sssw0101@naver.com>
2022-12-19 22:26:16 +0900

    불필요 어노테이션 제거

--- msa_e_commerce/user-service/src/main/java/com/example/userservice/repository/UserRepository.java
+++ msa_e_commerce/user-service/src/main/java/com/example/userservice/repository/UserRepository.java
@@ -4,7 +4,6 @@ import com.example.userservice.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
 
 }
