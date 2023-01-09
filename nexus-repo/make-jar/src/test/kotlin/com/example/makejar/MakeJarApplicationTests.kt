c5b969e45545d64e39c872c71663ee1f593bf20d
Kim yonggwan <sssw0101@naver.com>
2023-01-10 00:13:42 +0900

    Revert "delete make-jar"
    This reverts commit 4040c3fa79b3cf7d1f42dfd8c982b45dfe462938.

---
+++ nexus-repo/make-jar/src/test/kotlin/com/example/makejar/MakeJarApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.makejar
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MakeJarApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
