9368286add95ee68f2b61e560b8b454d5b380d88
Kim yonggwan <sssw0101@naver.com>
2023-01-16 00:04:59 +0900

    문제점

--- concurrency_stock/src/test/java/com/example/stock/service/StockServiceImplTest.java
+++ concurrency_stock/src/test/java/com/example/stock/service/StockServiceImplTest.java
@@ -4,9 +4,14 @@ import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -40,5 +45,36 @@ class StockServiceImplTest {
         Stock stock = stockRepository.findById(1L).orElseThrow();
 
         assertEquals(99, stock.getQuantity());
+    }
+
+    @Test
+    @DisplayName("동시에 100개의 요청 테스트")
+    public void hundredRequestTest() throws InterruptedException {
+        int threadCount = 100;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(32); // 비동기로 실행하는 작업을 단순화하도록 사용.
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for(int i = 0; i< threadCount; i++){
+            executorService.submit(()->{
+                try{
+                    stockServiceImpl.decreaseStock(1L, 1L);
+                }finally {
+                    latch.countDown(); // 100개의 쓰레드가 끝날때까지 카운팅
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        /**
+         * 레이스 컨디션
+         * 2개 이상의 쓰레드가 공유 데이터에 접근가능하고,
+         * 동시에 변경하는 경우 일어나는문제.
+         */
+        // test 실패
+        assertEquals(0L, stock.getQuantity());
     }
 }
