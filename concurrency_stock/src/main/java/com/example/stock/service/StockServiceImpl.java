3693280553c59a2ed26f1b464fce48b33364e41c
Kim yonggwan <sssw0101@naver.com>
2023-01-17 00:00:05 +0900

    synchronized 이용 - 주석 추가

--- concurrency_stock/src/main/java/com/example/stock/service/StockServiceImpl.java
+++ concurrency_stock/src/main/java/com/example/stock/service/StockServiceImpl.java
@@ -12,6 +12,10 @@ public class StockServiceImpl implements StockService{
 
     private final StockRepository stockRepository;
 
+    /**
+     * synchronized : 각 프로세스 안에서만 보장되기때문에,
+     * 여러 스레드에서 동시에 데이터 접근이 가능해지기때문에 레이스 컨디션 발생.
+     */
     @Override
     //@Transactional //stockService 새로 만들어서 해당 메소드 실행.
     public synchronized void decreaseStock(Long productId, Long quantity) {
