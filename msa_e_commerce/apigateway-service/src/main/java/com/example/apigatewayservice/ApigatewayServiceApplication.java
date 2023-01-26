f1fe0a08c77b8f4b73c0de273732c7cf8bf463bf
Kim yonggwan <sssw0101@naver.com>
2023-01-27 00:33:58 +0900

    Spring Cloud Gateway에서 Spring Cloud Config 연동

--- msa_e_commerce/apigateway-service/src/main/java/com/example/apigatewayservice/ApigatewayServiceApplication.java
+++ msa_e_commerce/apigateway-service/src/main/java/com/example/apigatewayservice/ApigatewayServiceApplication.java
@@ -1,7 +1,10 @@
 package com.example.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApigatewayServiceApplication {
@@ -10,4 +13,11 @@ public class ApigatewayServiceApplication {
         SpringApplication.run(ApigatewayServiceApplication.class, args);
     }
 
+    /**
+     * httpTrace 호출시, trace에 담긴 정보가 메모리에 담겨 endPoint로 제공.
+     */
+    @Bean
+    public HttpTraceRepository httpTraceRepository(){
+        return new InMemoryHttpTraceRepository();
+    }
 }
