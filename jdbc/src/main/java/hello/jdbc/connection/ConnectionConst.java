107a612135c25892910576af151061caa3556ee8
Kim yonggwan <sssw0101@naver.com>
2023-05-02 00:22:01 +0900

    스프링 DB 1편 - DataSource 적용

--- jdbc/src/main/java/hello/jdbc/connection/ConnectionConst.java
+++ jdbc/src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -4,7 +4,7 @@ package hello.jdbc.connection;
  * 상수를 모아둔것이기 때문에 객체 생성 막기위해 추상클래스 선언.
  */
 public abstract class ConnectionConst {
-    static final String URL = "jdbc:h2:tcp://localhost/~/test";
-    static final String USERNAME = "sa";
-    static final String PASSWORD = "";
+    public static final String URL = "jdbc:h2:tcp://localhost/~/test";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "";
 }
