2fd03c1fc346dcd55f71adca721ce7042c31e12f
Kim yonggwan <sssw0101@naver.com>
2023-08-13 20:41:20 +0900

    실전! 스프링 데이터 JPA - 순수 JPA 기반 리포지토리 만들기

--- data-jpa/src/main/kotlin/study/datajpa/repository/MemberJpaRepository.kt
+++ data-jpa/src/main/kotlin/study/datajpa/repository/MemberJpaRepository.kt
@@ -2,6 +2,7 @@ package study.datajpa.repository
 
 import org.springframework.stereotype.Repository
 import study.datajpa.entity.Member
+import java.util.Optional
 import javax.persistence.EntityManager
 import javax.persistence.PersistenceContext
 
@@ -14,6 +15,26 @@ class MemberJpaRepository {
     fun save(member: Member): Member{
         em.persist(member)
         return member
+    }
+
+    fun delete(member: Member){
+        em.remove(member)
+    }
+
+    fun findAll(): List<Member>{
+        //jpql 사용
+        return em.createQuery("select m from Member m", Member::class.java)
+                .resultList
+    }
+
+    fun findById(id: Long): Optional<Member>{
+        val member = em.find(Member::class.java, id)
+        return Optional.ofNullable(member)
+    }
+
+    fun count(): Long{
+        return em.createQuery("select count(m) from Member m", Long::class.javaObjectType)
+                .singleResult
     }
 
     fun find(id: Long): Member{
--- data-jpa/src/test/kotlin/study/datajpa/repository/MemberJpaRepositoryTest.kt
+++ data-jpa/src/test/kotlin/study/datajpa/repository/MemberJpaRepositoryTest.kt
@@ -1,6 +1,5 @@
 package study.datajpa.repository
 
-import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -28,5 +27,36 @@ internal class MemberJpaRepositoryTest{
         assertThat(findMember.id).isEqualTo(member.id)
         assertThat(findMember.username).isEqualTo(member.username)
         assertThat(findMember).isEqualTo(member)
+    }
+
+    @Test
+    fun basicCRUD(){
+        val member1 = Member("member1")
+        val member2 = Member("member2")
+
+        memberJpaRepository.save(member1)
+        memberJpaRepository.save(member2)
+
+        //단건 조회 검증.
+        val findMember1 = memberJpaRepository.findById(member1.id!!).get()
+        val findMember2 = memberJpaRepository.findById(member2.id!!).get()
+        assertThat(findMember1).isEqualTo(member1)
+        assertThat(findMember2).isEqualTo(member2)
+
+        //리스트 조회 검증
+        val all = memberJpaRepository.findAll()
+        assertThat(all.size).isEqualTo(2)
+
+        //카운트 검증
+        val count: Long = memberJpaRepository.count()
+        assertThat(count).isEqualTo(2L)
+
+        //삭제 검증
+        memberJpaRepository.delete(member1)
+        memberJpaRepository.delete(member2)
+
+        val deletedCount = memberJpaRepository.count()
+        assertThat(deletedCount).isEqualTo(0L)
+
     }
 }
